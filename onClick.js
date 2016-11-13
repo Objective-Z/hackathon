7d6c819b6b2ac0ed3b9e3eeafacf852c186f746b
Alex Zerphy <ajz5109@psu.edu>
2016-11-13 03:48:58 -0500

    Add files via upload

---
+++ onClick.js
@@ -0,0 +1,22 @@
+$('.walking').click(function(){
+	
+	$('.heart').val('80');
+	
+});
+
+$('.running').click(function(){
+	
+	$('.heart').val('120');
+	
+});
+
+$('.cycling').click(function(){
+	
+	$('.heart').val('170');
+	
+});
+
+$('.kayaking').click(function(){
+	
+	$('.heart').val('150');
+});
